c618c12f48238c8a3afbb275de02bdfa34d2835c
Nely1226 <nelyflores116@gmail.com>
2021-05-13 13:48:17 -0700

    n

--- js/homepagecopy.js
+++ js/homepagecopy.js
@@ -1,7 +1,1 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    alert("Here is some information on the Benefits of Mediation");
-  });
-});
-
-$("button").css("color","black");
+alert("")
